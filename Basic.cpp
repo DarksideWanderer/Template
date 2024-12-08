be2950f410caf451a3c8c465376942168e0bcbc5
DarksideWanderer <1726978428@qq.com>
2024-12-08 09:31:06 +0800

    dfsd

--- Basic.cpp
+++ Basic.cpp
@@ -14,6 +14,10 @@ void Clear(Args&... args){
 }
 std::default_random_engine E(std::chrono::steady_clock().now().time_since_epoch().count());
 
+void Main(int Case){
+	
+}
+
 int main(){
 	#ifdef LOCAL
 	freopen("In.txt","r",stdin);
@@ -21,7 +25,7 @@ int main(){
 	#endif
 	int Task=1;scanf("%d",&Task);
 	for(int Case=1;Case<=Task;Case++){
-		Main();
+		Main(Case);
 	}
 	return 0;
 }
