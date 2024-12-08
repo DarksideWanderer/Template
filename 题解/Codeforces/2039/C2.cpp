79a28b8c8d053e60a80f0bc2b04a618283d1c9e2
DarksideWanderer <1726978428@qq.com>
2024-12-08 19:21:54 +0800

    dfsafds

--- 题解/Codeforces/2039/C2.cpp
+++ 题解/Codeforces/2039/C2.cpp
@@ -14,7 +14,28 @@ void Clear(Args&... args){
 }
 std::default_random_engine E(std::chrono::steady_clock().now().time_since_epoch().count());
 
+int x;ll m;
 void Main(int Case){
+	scanf("%d%lld",&x,&m);
+	int k=log2(x);int t=(1<<(k+1))-1;
+	ll ans=0;
+	for(int i=1;i<=t;i++){//y=1~t
+		if((x^i)%i==0||(x^i)%x==0){
+			if(i<=m)ans++;
+		}
+	}
+	
+	if(m>t){
+		ll s=m|t;//最大范围
+		ll p=s-t;
+		ll r=p-1;
+		ans+=(r/x)-(t/x);
+		for(ll y=p;y<=m;y++){
+			if((y^x)%x==0&&y)ans++;
+		}
+	}
+	
+	printf("%lld\n",ans);
 }
 
 int main(){
