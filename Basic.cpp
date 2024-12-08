47cabb1cbc5d0b7ad868570e918649725af7ad33
DarksideWanderer <1726978428@qq.com>
2024-12-08 10:15:40 +0800

    Worker (#20)
    * 04
    
    * dfsadf
    
    * 0
    
    * dfs
    
    * Update c_cpp_properties.json
    
    * dfsd
    
    * dfsdf

--- Basic.cpp
+++ Basic.cpp
@@ -14,6 +14,11 @@ void Clear(Args&... args){
 }
 std::default_random_engine E(std::chrono::steady_clock().now().time_since_epoch().count());
 
+void Main(int Case){
+	int n;scanf("%d",&n);
+	printf("%d\n",n*n);
+}
+
 int main(){
 	#ifdef LOCAL
 	freopen("In.txt","r",stdin);
@@ -21,7 +26,7 @@ int main(){
 	#endif
 	int Task=1;scanf("%d",&Task);
 	for(int Case=1;Case<=Task;Case++){
-		Main();
+		Main(Case);
 	}
 	return 0;
 }
